1c6e7551f3c531a316743a6cbb0b604a3f3f1e32
Jordan Lee <jordanlee2814@gmail.com>
2023-05-01 16:25:41 -0600

    add files to repo

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  //reactStrictMode: true, // uncomment for node testing locally
+}
+
+module.exports = nextConfig
