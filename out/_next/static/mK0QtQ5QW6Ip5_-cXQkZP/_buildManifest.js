c693b2b29cb2865428f41bed0c4368ae2b4ae481
Jordan Lee <jordanlee2814@gmail.com>
2023-05-02 15:43:18 -0600

    prepare for AWS hosting
    npm run build, prep an index.html file to use the index.js

---
+++ out/_next/static/mK0QtQ5QW6Ip5_-cXQkZP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-bcf0ba6d4f231caf.js","static/chunks/d7eeaac4-5486f34730382d4c.js","static/chunks/pages/index-5d747c60a06be652.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
