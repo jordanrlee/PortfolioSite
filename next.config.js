728a17f3a06e16b36878a5c0c17998ca00b82af5
Jordan Lee <jordanlee2814@gmail.com>
2023-05-09 11:05:13 -0600

    build project for AWS
    quick npm run build for hosting on S3 bucket

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //output: 'export',
-  reactStrictMode: true, // uncomment for node testing locally
+  output: 'export',
+  //reactStrictMode: true, // uncomment for node testing locally
 }
 
 module.exports = {
